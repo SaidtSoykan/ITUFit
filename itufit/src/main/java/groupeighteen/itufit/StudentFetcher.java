9b3ba9ccb517046fca4933c58e1456e4dfea6f9d
erdoganer19 <73936365+ituitis20-erdoganer19@users.noreply.github.com>
2023-12-13 00:08:56 +0300

    StudentFetcher

---
+++ itufit/src/main/java/groupeighteen/itufit/StudentFetcher.java
@@ -0,0 +1,42 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentFetcher {
+
+    public static Student fetchStudent(Connection connection, String username, String password) {
+        // SQL sorgusu
+        String sql = "SELECT * FROM students WHERE username = ? AND password = ?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            // Parametreleri ayarla
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    // Veritabanından alınan verilerle Student nesnesi oluştur
+                    Student student = new Student();
+                    student.setStudentId(resultSet.getInt("student_id"));
+                    student.setName(resultSet.getString("name"));
+                    student.setSurname(resultSet.getString("surname"));
+                    student.setGender(resultSet.getString("gender"));
+                    student.setMatchedStudentId(resultSet.getInt("matched_student_id"));
+                    student.setRestrictionViolationScore(resultSet.getInt("restriction_violation_score"));
+                    student.setPassword(resultSet.getString("password"));
+                    student.setEmail(resultSet.getString("email"));
+                    student.setWeight(resultSet.getDouble("weight"));
+                    student.setGoalWeight(resultSet.getDouble("goal_weight"));
+
+                    return student;
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null; // Kullanıcı bulunamazsa null döndür
+    }
+}
